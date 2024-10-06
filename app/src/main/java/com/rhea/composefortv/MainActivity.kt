967495d7d4be27619ecf1681aa4a7170f738dbf3
chhabra <rheachhabra1011@gmail.com>
2024-10-06 21:52:22 +0530

    init project

---
+++ app/src/main/java/com/rhea/composefortv/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.rhea.composefortv
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.tv.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.tv.material3.ExperimentalTvMaterial3Api
+import androidx.tv.material3.Surface
+import com.rhea.composefortv.ui.theme.ComposeForTVTheme
+
+class MainActivity : ComponentActivity() {
+  @OptIn(ExperimentalTvMaterial3Api::class)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      ComposeForTVTheme {
+        Surface(
+          modifier = Modifier.fillMaxSize(),
+          shape = RectangleShape
+        ) {
+        }
+      }
+    }
+  }
+}
---
+++ app/src/main/java/com/rhea/composefortv/ui/AppNavHost.kt
@@ -0,0 +1,12 @@
+package com.rhea.composefortv.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavHost(
+  navController: NavController = rememberNavController()
+) {
+
+}
